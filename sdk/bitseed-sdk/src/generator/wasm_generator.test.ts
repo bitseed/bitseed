7c140b0d2a776258d1adc4773576ac4bf5b30197
Owen Wu <1449069+yubing744@users.noreply.github.com>
2024-03-13 11:52:04 +0800

    Owen/rust generator with static mem alloc (#14)
    * teat: test rust generator
    
    * feat: min generator.wasm
    
    * feat: compile rust generator ok
    
    * feat: rust generator status alloc
    
    * static allc fail
    
    * feat: add constants and types mod
    
    * feat: add unit test for generator
    
    * feat: input data encode and output data decode
    
    * feat: call rust generator ok
    
    * feat: ok for e2e test
    
    * feat: ok for release generator
    
    * feat: impl inscribe_verify and indexer_generate
    
    * feat: upload rust generator-bg.wasm

--- sdk/bitseed-sdk/src/generator/wasm_generator.test.ts
+++ sdk/bitseed-sdk/src/generator/wasm_generator.test.ts
@@ -37,4 +37,30 @@ describe('WasmGenerator', () => {
     expect(result).toHaveProperty("attributes");
     expect(result).toHaveProperty("content");
   });
+
+  it('should call inscribe_generate with correct parameters with rust generator', async () => {
+    // Create an instance of WasmGenerator
+    const generator = await loadWasmModuleFromFile(path.resolve(__dirname, '../../tests/data/generator-rust.wasm'))
+
+    // Prepare test data
+    const deployArgs = [
+      `{"height":{"type":"range","data":{"min":1,"max":1000}}}`
+    ];
+
+    const seed = new InscribeSeed('testSeed', {
+      txid: '75e95eeba0b3450feda8d880efe00600816e5934160a4757fbdaa99a0e3bb436',
+      vout: 1,
+    });
+    
+    const userInput = 'testUserInput';
+
+    // Call the inscribeGenerate method
+    const result = await generator.inscribeGenerate(deployArgs, seed, userInput);
+    console.log('result:', result)
+
+    // Assert that result has properties "id" and "amount"
+    expect(result).toHaveProperty("amount");
+    expect(result).toHaveProperty("attributes");
+    expect(result).toHaveProperty("content");
+  });
 });
